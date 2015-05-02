34c14d6de1b126bc9e0c8f246114dc4387f96630
Ryan Gorsuch <ryan.gorsuch@gmail.com>
2015-05-02 18:44:42 +0000

    Added git reminder

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ cp screenrc ~/.screenrc
 cp vimrc ~/.vimrc
 cp bashrc ~/.bashrc_local
 echo ". ~/.bashrc_local" >> ~/.bashrc
+
+echo "Do not forget to "
+echo
+echo '  git config --global user.name "User Name"'
+echo '  git config --global user.email you@mail.com'
+echo
